c75141c924163569a685465903773761a8b097fa
manjii <akswlakswl1@gmail.com>
2026-06-04 22:27:38 +0900

    feat: GET /progress   엔드포인트 - progress.md 반환

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ app = FastAPI()
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+@app.get("/progress")
+def progress():
+    with open("progress.md", "r") as f:
+      content = f.read()
+    return {"content":content}
